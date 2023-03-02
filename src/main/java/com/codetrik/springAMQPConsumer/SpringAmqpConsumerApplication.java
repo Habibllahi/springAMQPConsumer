5ff2b92d74b2c429aeafdda4cea6687edef6ba79
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2023-03-02 15:05:55 +0100

    second commit

---
+++ src/main/java/com/codetrik/springAMQPConsumer/SpringAmqpConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.codetrik.springAMQPConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmqpConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAmqpConsumerApplication.class, args);
+	}
+
+}
