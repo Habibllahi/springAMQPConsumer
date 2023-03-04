e182f66bfa2537e2297f7f0a6448afda1c518e6f
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2023-03-04 13:57:00 +0100

    update

--- src/main/java/com/codetrik/springAMQPConsumer/amqp/SimpleAnnotatedConsumer.java
+++ src/main/java/com/codetrik/springAMQPConsumer/amqp/SimpleAnnotatedConsumer.java
@@ -1,7 +1,13 @@
 package com.codetrik.springAMQPConsumer.amqp;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class SimpleAnnotatedConsumer {
@@ -9,5 +15,14 @@ public class SimpleAnnotatedConsumer {
     @RabbitListener(queues = {"${com.codetrik.amqp.queue}"})
     public void consume(String message){
         System.out.println("annotatedConsumer>> "+message);
+    }
+
+    @RabbitListener(queues = {"${com.codetrik.amqp.rpc-queue}"})
+    @SendTo({"${com.codetrik.amqp.reply-to-queue}"})
+    public Message rpcConsume(Message message){
+        System.out.println("annotatedRPCConsumer>> "+new String(message.getBody()));
+        return MessageBuilder.withBody("Acknowledge".getBytes(StandardCharsets.UTF_8))
+                .setCorrelationId(message.getMessageProperties().getCorrelationId())
+                .build();
     }
 }
--- src/main/java/com/codetrik/springAMQPConsumer/config/AMQPConfig.java
+++ src/main/java/com/codetrik/springAMQPConsumer/config/AMQPConfig.java
@@ -1,12 +1,16 @@
 package com.codetrik.springAMQPConsumer.config;
 
 import com.codetrik.springAMQPConsumer.amqp.SimpleConsumer;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionListener;
 import org.springframework.amqp.rabbit.connection.PooledChannelConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,11 +42,33 @@ public class AMQPConfig {
 
 
     //Create a Queue
-    @Bean
+    //Create a Queue
+    @Bean("just-queue")
     public Queue createQueue(@Value("${com.codetrik.amqp.queue}") String name){
         return new Queue(name,false);
     }
 
+    @Bean("rpc-queue")
+    public Queue createRpcQueue(@Value("${com.codetrik.amqp.rpc-queue}") String name){
+        return new Queue(name,false);
+    }
+
+    @Bean("reply-to-queue")
+    public Queue createReplyToQueue(@Value("${com.codetrik.amqp.reply-to-queue}") String name){
+        return new Queue(name,false);
+    }
+
+    @Bean("rpc-direct")
+    public DirectExchange directExchange(@Value("${com.codetrik.amqp.direct-exchange}") String name){
+        return new DirectExchange(name,false,false);
+    }
+
+    @Bean
+    public Binding rpcQueueBindToDirectExchange(@Qualifier("rpc-queue") Queue rpcQueue,
+                                                @Qualifier("rpc-direct") DirectExchange rpcDirectExchange,
+                                                @Value("${com.codetrik.amqp.router}") String routeKey){
+        return BindingBuilder.bind(rpcQueue).to(rpcDirectExchange).with(routeKey);
+    }
     //This bean is necessary when not using @RabbitListener annotation in creating listener
     @Bean
     public SimpleMessageListenerContainer container(@Value("${com.codetrik.amqp.queue}") String queue, ConnectionFactory cf,
